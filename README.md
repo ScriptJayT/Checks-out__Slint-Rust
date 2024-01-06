a5605694ba13fa9db0dce38bc10eed7ff7198052
Jace <jt.scripter@gmail.com>
2024-01-06 16:56:01 +0100

    Updated README + Extracted Comp

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # A project started
+
+- Password Keeper
+    - save encrypted in file
+    - add tag
+    - search tag
+    - see & copy after pw
+    - encrypt salt in user-file
+- Password Generator
+- *DoIt*
+    a todo list that would send a notification to initiate
+    - db connection / api connection
+    - set new item, hour if same day, otherwise date
+    - settings
+        - default hour of notification
+        - tags to follow
+    - save settings in user-file
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
+use slint::SharedString;
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
-
     let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
-        let ui = ui_handle.unwrap();
+    
+    ui.on_close_feedback(move || {
+        let ui: AppWindow = ui_handle.unwrap();
+        ui.set_feedback_out("feedback reset".into());
+        ui.set_is_open(false.into());
+    });
+    
+    let ui_handle = ui.as_weak();
+    ui.on_set_feedback(move |_msg: SharedString| {
+        let ui: AppWindow = ui_handle.unwrap();
+        ui.set_feedback_out(_msg.into());
+        ui.set_is_open(true.into());
     });
 
     ui.run()
