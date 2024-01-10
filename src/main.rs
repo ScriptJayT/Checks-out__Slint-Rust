d3cf97df3b4bdb4a00105f69dcefb82f86d5ecb0
Jace <jt.scripter@gmail.com>
2024-01-10 18:05:43 +0100

    added descr to pw save

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
+
+    // user feedback
     let ui_handle = ui.as_weak();
-    
     ui.on_close_feedback(move || {
         let ui: AppWindow = ui_handle.unwrap();
         ui.set_feedback_out("feedback reset".into());
         ui.set_is_open(false.into());
     });
-    
     let ui_handle = ui.as_weak();
     ui.on_set_feedback(move |_msg: SharedString| {
         let ui: AppWindow = ui_handle.unwrap();
@@ -19,16 +19,20 @@ fn main() -> Result<(), slint::PlatformError> {
         ui.set_is_open(true.into());
     });
     
+    // save & encrypt passwords
     let ui_handle = ui.as_weak();
-    ui.on_save_password(move |_ref: SharedString, _pw: SharedString| {
-        let cleaned_ref: &str = _ref.trim();
-        let cleaned_pw: &str = _pw.trim();
-        let valid_ref: bool = cleaned_ref.chars().count() > 1;
-        let valid_pw: bool = cleaned_pw.chars().count() > 1;
-
+    ui.on_save_password(move |_ref: SharedString, _pw: SharedString, _descr: SharedString| {
         let mut has_error: bool = false;
         let mut error: [_; 2] = ["", ""];
+        
+        //? clean values
+        let cleaned_ref: &str = _ref.trim();
+        let cleaned_pw: &str = _pw.trim();
+        let cleaned_descr: &str = _descr.trim();
 
+        //? check if valid inputs
+        let valid_ref: bool = cleaned_ref.chars().count() > 1;
+        let valid_pw: bool = cleaned_pw.chars().count() > 1;
         if !valid_ref  {
             has_error = true;
             error[0] = " - `Reference` needs to be filled in";
@@ -38,13 +42,17 @@ fn main() -> Result<(), slint::PlatformError> {
             error[1] = " - `Password` needs to be filled in";
         }
         
+        //? return & reply
         let ui: AppWindow = ui_handle.unwrap();
         if has_error {
             let res: String = format!("Error: \n{}", error.join("\n").trim());
             ui.set_feedback_out(res.into());
         }
         else {
-            let res: String = format!("Password saved for: `{}` \n ({})", cleaned_ref, cleaned_pw);
+            let res: String = format!(
+                                "Password saved for: `{}`\n({})\nwith:\n{}", 
+                                cleaned_ref, cleaned_pw, cleaned_descr
+                            );
             ui.set_feedback_out(res.into());
         }
         
