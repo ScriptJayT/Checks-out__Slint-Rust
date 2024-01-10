445cf65ac00514e2be9419a087073c67780d4661
Jace <jt.scripter@gmail.com>
2024-01-10 17:50:10 +0100

    setup  pw feedback + restyle window

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,38 @@ fn main() -> Result<(), slint::PlatformError> {
         ui.set_feedback_out(_msg.into());
         ui.set_is_open(true.into());
     });
+    
+    let ui_handle = ui.as_weak();
+    ui.on_save_password(move |_ref: SharedString, _pw: SharedString| {
+        let cleaned_ref: &str = _ref.trim();
+        let cleaned_pw: &str = _pw.trim();
+        let valid_ref: bool = cleaned_ref.chars().count() > 1;
+        let valid_pw: bool = cleaned_pw.chars().count() > 1;
+
+        let mut has_error: bool = false;
+        let mut error: [_; 2] = ["", ""];
+
+        if !valid_ref  {
+            has_error = true;
+            error[0] = " - `Reference` needs to be filled in";
+        }
+        if !valid_pw  {
+            has_error = true;
+            error[1] = " - `Password` needs to be filled in";
+        }
+        
+        let ui: AppWindow = ui_handle.unwrap();
+        if has_error {
+            let res: String = format!("Error: \n{}", error.join("\n").trim());
+            ui.set_feedback_out(res.into());
+        }
+        else {
+            let res: String = format!("Password saved for: `{}` \n ({})", cleaned_ref, cleaned_pw);
+            ui.set_feedback_out(res.into());
+        }
+        
+        ui.set_is_open(true.into());
+    });
 
     ui.run()
 }
